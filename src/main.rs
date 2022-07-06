c2d2c6851e7c3e29f578703477a4d517b25ca55e
Tom Xiong <tomxiongzh@gmail.com>
2022-07-07 01:02:41 +0800

    1. add exit command to quit 2. add echo message to client 3. add print the input to server console

--- src/main.rs
+++ src/main.rs
@@ -5,50 +5,74 @@ use std::net::{TcpListener, TcpStream};
 fn main() -> std::io::Result<()> {
     let tl = TcpListener::bind("0.0.0.0:12345")?;
 
-   /* match tl.accept() {
-        Ok((_socket, addr)) => println!("This is new client from {addr:?}"),
-        Err(e) => println!("Failed to get client : {e:?}"),
-    }*/
-    for stream in tl.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_stream(&stream);
-            }
-            Err(e)=> {
-                println!("Error for incoming stream: {e:?}");
-                panic!("Panic for {e:?}");
+    loop {
+        for stream in tl.incoming() {
+            match stream {
+                Ok(stream) => {
+                    match handle_stream(&stream) {
+                        Ok(ret) => {
+                            if ret == 0 {
+                                break;
+                            } else {
+                                continue;
+                            }
+                        },
+                        Err(e) =>
+                            println!("Handel stream Error: {e:?}")
+                    }
+                }
+                Err(e)=> {
+                    println!("Error for incoming stream: {e:?}");
+                    panic!("Panic for {e:?}");
+                }
             }
         }
     }
-    //println!("Hello, world!");
-    Ok(())
+    //Ok(())
 }
 
-fn handle_stream(mut stream: &TcpStream) -> Result<(), Error> {
-    let mut rbuf = [0;50];
+fn handle_stream(mut stream: &TcpStream) -> Result<u8, Error> {
     loop {
+        let mut rbuf = [0;50];
         let br = stream.read(&mut rbuf);
         match br {
             Ok(bl) => {
                 if bl == 0 {
-                    return Ok(())
+                    return Ok(0)
                 }
             },
-            Err(e) => println!("error : {e:?}"),
+            Err(e) =>
+                println!("stream read error : {e:?}"),
         }
-        stream.write(&rbuf);
+        if !rbuf.is_ascii() {
+            let reps = String::from("Invalid character");
+            println!("From {} say {}", stream.peer_addr().unwrap(), reps);
+            stream.write(&rbuf);
+            continue
+        }
+
         let cmd = String::from_utf8(rbuf.to_vec());
         match cmd {
             Ok(cmds) => {
-                let cmdst = cmds.trim_end();
-                if cmdst == "exit" {
-                    return Ok(())
+                let cmdst = cmds.trim_end().lines().next();
+                println!("From {} say {}", stream.peer_addr().unwrap(), cmdst.unwrap());
+                if cmdst == Some("exit") {
+                    return Ok(0);
+                }
+                let wr = stream.write(&rbuf);
+                match wr {
+                    Ok(wl) => {
+                        if wl == 0 {
+                            return Ok(0)
+                        }
+                    }
+                    Err(e) =>
+                        println!("Failed to write back: {e:?}")
                 }
             },
-            Err(e) => println!("error : {e:?}"),
+            Err(e) => println!("parse input to string error : {e:?}")
         }
-        let mut rbuf = [0;50];
-        //println!("rec message: {command:?}");
+
+
     }
-    //Ok(())
 }
